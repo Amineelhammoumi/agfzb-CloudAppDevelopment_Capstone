cd1835de420daa148d62121ee2e597c23f46beb1
Amineelhammoumi <amine.e.hammoumi@gmail.com>
2023-04-03 12:55:31 +0000

    added Auth

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -9,16 +9,19 @@ urlpatterns = [
     # view refers to the view function
     # name the URL
 
-    path(route='static/', view=views.static, name='static'),
+    path(route='base/', view=views.base, name='base'),
     # path for about view
     path(route='about/', view=views.about, name='about'),
     # path for contact us view
     path(route='contact/', view=views.contact, name='contact'),
-    # path for registration
+       # path for registration
+    path(route='registration/', view=views.registration_request, name='registration'),
 
     # path for login
+    path(route='login/', view=views.login_request, name='login'),
 
     # path for logout
+    path(route='logout/', view=views.logout_request, name='logout'),
 
     path(route='', view=views.get_dealerships, name='index'),
 
