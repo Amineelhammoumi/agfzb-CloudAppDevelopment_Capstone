ab1401f2deee4e78858b0b95205a497fb97fbe56
Amineelhammoumi <amine.e.hammoumi@gmail.com>
2023-04-06 14:00:45 +0000

    added views

--- functions/sample/python/main.py
+++ functions/sample/python/main.py
@@ -20,8 +20,8 @@ def main(param_dict):
 
     try:
         client = Cloudant.iam(
-            account_name=param_dict["COUCH_USERNAME"],
-            api_key=param_dict["IAM_API_KEY"],
+            account_name=param_dict["https://apikey-v2-362iuztsewhc5fyvsqz1x5qizsty4clva6wogfsrv1e9:0b2374ecb5a964bb0870d89b5189a32a@f4269b51-8a28-4d6f-9d9d-906b353c80bc-bluemix.cloudantnosqldb.appdomain.cloud"],
+            api_key=param_dict["X6cV4GxKuDhfilvjWO1SJHhet_jRmJ8Jo1KAckvDY-Cy"],
             connect=True,
         )
         print(f"Databases: {client.all_dbs()}")
